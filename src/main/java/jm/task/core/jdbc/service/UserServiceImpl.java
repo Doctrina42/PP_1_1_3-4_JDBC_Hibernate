25830d158545c565ede238e0d1c53502b773c86b
Doctrina42 <107268550+Doctrina42@users.noreply.github.com>
2023-09-13 21:53:17 +0700

    Add files via upload

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USER_NAME = "root";
